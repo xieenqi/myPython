5f4d75a9fcde9e9a58fc56ddc55079cdd315e657
xieenqi <xieenqi@dgg.net>
2019-01-16 18:15:33 +0800

    tiji

--- onePython.py
+++ onePython.py
@@ -15,3 +15,11 @@ for i in range(1,10):
         print('%s*%s=%s' %(i,j,i*j),end=' \t ')
     print()
 
+print(''' wiod fsfndsf 'This' is a multi-line string. This is the first line. This is the second line.
+"What's your name?," I asked.
+He said "Bond, James Bond." ''')
+
+print('''What\'s your name?''')
+
+print("Newlines are indicated by \n")
+print(r"Newlines are indicated by \n")
