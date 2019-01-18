0fb81fab7ce3d11d927aa9c043815fdd989a3856
xieenqi <xieenqi@dgg.net>
2019-01-18 15:19:59 +0800

    流程控制

--- twoPython.py
+++ twoPython.py
@@ -1,3 +1,5 @@
+from pip._vendor.distlib.compat import raw_input
+
 coke=0
 hen=0
 chieck=0
@@ -9,3 +11,14 @@ for x in range(0,20+2):
             zz=z/3
             if sum==100 and count==100:
                 print('公鸡：'+str(x)+',母鸡：'+str(y)+'小鸡：'+str(z))
+
+# if else  流程控制
+number = 23
+guess = int(raw_input('Enter an integer : '))
+if guess == number:
+    print ('Congratulations, you guessed it.') # New block starts here print "(but you do not win any prizes!)" # New block ends here
+elif guess < number:
+    print ('No, it is a little higher than that') # Another block # You can do whatever you want in a block ...
+else:
+    print ('No, it is a little lower than that')
+# you must have guess > number to reach here
