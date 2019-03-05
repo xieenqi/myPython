93aca0c88cf41ca77b4173b7743c7e754a0e9321
xieenqi <xieenqi@dgg.net>
2019-03-05 17:52:34 +0800

    文件夹备份成zip

--- script.py
+++ script.py
@@ -2,10 +2,12 @@
 import os
 import time
 # 1. The files and directories to be backed up are specified in a list.
-source = ['/home/swaroop/byte', '/home/swaroop/bin']
+# 要备份的文件和目录在列表中指定  /Users/dgg2/juno/WX
+source = ['/Users/dgg2/juno/WX', '/Users/dgg2/juno/IdeaProjects']
 # If you are using Windows, use source = [r'C:\Documents', r'D:\Work'] or something like that
-# 2. The backup must be stored in a main backup directory
-target_dir = '/mnt/e/backup/' # Remember to change this to what you will be using
+# 2. The backup must be stored in a main backup directory 备份必须存储在主备份目录中
+target_dir = '/Users/dgg2/juno/Flutter/NDN'
+# Remember to change this to what you will be using
 # 3. The files are backed up into a zip file.
 # 4. The name of the zip archive is the current date and time
 target = target_dir + time.strftime('%Y%m%d%H%M%S') + '.zip'
